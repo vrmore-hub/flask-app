447a1157fb5569fb428654bf08a5f8dfc5c2e606
Vidya <vidya@gmail.com>
2026-04-18 10:16:18 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,5 @@ def home():
     return "Hello World from Cloud 🚀"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
